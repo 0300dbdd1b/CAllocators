a77ea6d567293df69d9e5391e68ad995e7478f2b
0300dbdd1b <noah.gr.addino@gmail.com>
2025-03-11 16:28:08 +0100

    add alignment on arenas

--- linear-allocator.h
+++ linear-allocator.h
@@ -1,8 +1,14 @@
-
 #ifndef LINEAR_ALLOCATOR_H
 #define LINEAR_ALLOCATOR_H
 
+#include <cstdint>
 #include <stdlib.h>
+
+#if defined(__x86_64__)
+	#define WORD_SIZE 8
+#else
+	#define WORWORD_SIZE 4
+#endif
 
 typedef struct LinearAllocator
 {
@@ -10,9 +16,17 @@ typedef struct LinearAllocator
 	size_t  capacity;
 	size_t  usedMemory;
 } LinearAllocator;
-
 typedef LinearAllocator Arena;
 typedef LinearAllocator ArenaAllocator;
+
+
+static inline void *_align_forward(void *ptr, size_t alignment)
+{
+	uintptr_t 	addr	= (uintptr_t)ptr;
+	uintptr_t	aligned	= (addr + (alignment - 1)) & ~(alignment - 1);
+	return (void *)aligned;
+}
+
 
 static inline LinearAllocator *CreateLinearAllocator(size_t size)
 {
@@ -30,20 +44,23 @@ static inline LinearAllocator *CreateLinearAllocator(size_t size)
 	allocator->usedMemory = 0;
 	return allocator;
 }
-#define CreateArena CreateLinearAllocator
-#define CreateArenaAllocator CreateLinearAllocator
+#define CreateArena				CreateLinearAllocator
+#define CreateArenaAllocator	CreateLinearAllocator
 
 static inline void *AllocateFromLinearAllocator(LinearAllocator *allocator, size_t size)
 {
-	if (!allocator || size + allocator->usedMemory > allocator->capacity)
-		return NULL;
-
-	void *ptr = allocator->memory + allocator->usedMemory;
-	allocator->usedMemory += size;
-	return ptr;
+    if (!allocator) return NULL;
+    void	*curr_pos			= allocator->memory + allocator->usedMemory;
+    void	*aligned_pos		= _align_forward(curr_pos, WORD_SIZE);
+    size_t	alignment_offset	= (char *)aligned_pos - (char *)curr_pos;
+    size_t	total_required		= alignment_offset + size;
+    if (allocator->usedMemory + total_required > allocator->capacity) return NULL;
+    allocator->usedMemory += total_required;
+    return (aligned_pos);
 }
-#define AllocateFromArena AllocateFromLinearAllocator
-#define AllocateFromArenaAllocator AllocateFromLinearAllocator
+
+#define AllocateFromArena			AllocateFromLinearAllocator
+#define AllocateFromArenaAllocator	AllocateFromLinearAllocator
 
 static inline void FreeSizeFromLinearAllocator(LinearAllocator *allocator, size_t size)
 {
@@ -52,8 +69,8 @@ static inline void FreeSizeFromLinearAllocator(LinearAllocator *allocator, size_
 
 	allocator->usedMemory -= size;
 }
-#define FreeSizeFromArena FreeSizeFromLinearAllocator
-#define FreeSizeFromArenaAllocator FreeSizeFromLinearAllocator
+#define FreeSizeFromArena			FreeSizeFromLinearAllocator
+#define FreeSizeFromArenaAllocator	FreeSizeFromLinearAllocator
 
 static inline void FreeLinearAllocator(LinearAllocator *allocator)
 {
@@ -62,8 +79,8 @@ static inline void FreeLinearAllocator(LinearAllocator *allocator)
 
 	allocator->usedMemory = 0;
 }
-#define FreeArena FreeLinearAllocator
-#define FreeArenaAllocator FreeLinearAllocator
+#define FreeArena				FreeLinearAllocator
+#define FreeArenaAllocator		FreeLinearAllocator
 
 static inline void DestroyLinearAllocator(LinearAllocator *allocator)
 {
@@ -75,8 +92,8 @@ static inline void DestroyLinearAllocator(LinearAllocator *allocator)
 	allocator->capacity = 0;
 	allocator->usedMemory = 0;
 }
-#define DestroyArena DestroyLinearAllocator
-#define DestroyArenaAllocator DestroyLinearAllocator
+#define DestroyArena			DestroyLinearAllocator
+#define DestroyArenaAllocator	DestroyLinearAllocator
 
 #endif // LINEAR_ALLOCATOR_H
 
